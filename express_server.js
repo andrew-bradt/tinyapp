e6f41fca0f7304db61b8bba1075eb3faa6da4b23
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-22 16:32:39 -0500

    Added templateVars for GET request made to /urls/new

--- express_server.js
+++ express_server.js
@@ -41,7 +41,8 @@ app.post('/urls', (req, res)=>{
 
 
 app.get('/urls/new', (req, res)=>{
-  res.render('urls_new');
+  const templateVars = {username: req.cookies['username']};
+  res.render('urls_new', templateVars);
 });
 
 
