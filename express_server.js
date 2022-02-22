303b5a363b1105e9e534edd283b30d32fcf760a0
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-21 21:00:33 -0500

    implemented POST method for /urls/:shortURL - change longURL for given shortURL in databaseURL

--- express_server.js
+++ express_server.js
@@ -49,6 +49,13 @@ app.get('/urls/:shortURL', (req, res)=>{
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL', (req, res)=>{
+  const {shortURL} = req.params;
+  const {longURL} = req.body;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls');
+});
+
 app.post('/urls/:shortURL/delete', (req, res)=>{
   const {shortURL} = req.params;
   delete urlDatabase[shortURL];
