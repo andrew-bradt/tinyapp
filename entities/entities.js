7c5b0526fdff8ac57745f7209121f499db734672
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-24 14:37:27 -0500

    Fully implemented Visit and URL Classes, made BlankAnalytics Helper Class for initializing/resetting analytics for a URL

--- entities/entities.js
+++ entities/entities.js
@@ -1,3 +1,11 @@
+class BlankAnalytics {
+  constructor() {
+    this.totalVisits = 0;
+    this.uniqueVisitors = [];
+    this.visitorLog = [];
+  }
+}
+
 class Visit {
   constructor({timestamp, visitorID}) {
     this.timestamp = timestamp;
@@ -7,31 +15,42 @@ class Visit {
 
 class URL {
   constructor({userID, longURL}) {
-    this.longURL = longURL;
+    this._longURL = longURL;
     this.userID = userID;
-    this._analytics = {
-      totalVisits : 0,
-      uniqueVisitors: {},
-      visitorLog: []
-    };
+    this._analytics = new BlankAnalytics();
+  }
+  get longURL() {
+    return this._longURL;
+  }
+  set longURL (newURL) {
+    this._longURL = newURL;
+    this._resetAnalytics();
   }
   get uniqueVisitors() {
-    return Object.keys(this._analytics.uniqueVisitors).length;
+    return this.uniqueVisitors.length;
   }
-  onVisit(visit) {
-
+  addVisit(visit) {
+    this._incrementTotalVisits();
+    this._addUniqueVisitor(visit.visitorID);
+    this._appendVisitorLog(visit);
   }
-  _isNewVisitor() {
-    
+  _isNewVisitor(visitorID) {
+    return this._analytics.uniqueVisitors.filter(id => visitorID === id).length === 0;
   }
   _addUniqueVisitor(visitorID) {
-
+    if (this._isNewVisitor(visitorID)) {
+      this._analytics.uniqueVisitors.push(visitorID);
+      return true;
+    }
   }
   _incrementTotalVisits() {
     this._analytics.totalVisits++;
   }
-  _addVisit(visit) {
+  _appendVisitorLog(visit) {
     this._analytics.visitorLog.push(visit);
+  }
+  _resetAnalytics() {
+    this._analytics = new BlankAnalytics;
   }
 }
 
