c358437697d7f31d4d0f49a7e4b31d1d0e61dbb5
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-24 19:57:43 -0500

    Removed data from database objects

--- databases.js
+++ databases.js
@@ -1,17 +1,6 @@
 const urlDatabase = {};
 
-const users = {
-  user3t402k: {
-    userID: 'user3t402k',
-    email: 'test@gmail.com',
-    password: '$2a$10$iuFCzjzZi27Y6gZwF/bS4.dA6LI1Pau6O/iaf261mfbZdA0FdQ9Q2'
-  },
-  user999999: {
-    userID: 'user999999',
-    email: 'a@a.com',
-    password: '$2a$10$EnVwRvzz49NjQb7DgkTBjen4Be8fX9dh6fdprTzY/Vboyw/ALx04y'
-  }
-};
+const users = {};
 
 module.exports = {
   users,
